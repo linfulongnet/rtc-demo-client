62dc636435c9a73320c764a166b0e92771488371
linfl-bfdx <linfl@bfdx.com>
2019-07-02 17:16:34 +0800

    使用webSocket作为信令传输指令

--- src/components/Recording.vue
+++ src/components/Recording.vue
@@ -14,325 +14,399 @@
       <audio :src="source.dataUrl" controls></audio>
       <button @click="saveAsRecordData(source)">download</button>
     </div>
+
+    <div class='user-info' style='text-align: left;color:#a020ff'>
+      <span style='padding:0 10px'>current:</span>
+      <span style='padding:0 10px'>id: {{userInfo.id}}</span>
+      <span style='padding:0 10px'>onlineTime: {{userInfo.onlineTime}}</span>
+    </div>
+    <div class='rtc-connection-wrap'
+         v-if='wsList.length'>
+      <table class='online-users'>
+        <thead>
+        <tr>
+          <th>id</th>
+          <th>onlineTime</th>
+          <th>call</th>
+          <th>hang up</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for='(user,index) in wsList'
+            :key='index'>
+          <td v-for='(val,idx) in user'
+              :key='idx'>
+            {{val}}
+          </td>
+          <td>
+            <button @click='callPeer(user)'>call</button>
+          </td>
+          <td>
+            <button @click='hangUpPeer(user)'>hang up</button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   </section>
 </template>
 
 <script lang="ts">
-  import {Component, Vue} from 'vue-property-decorator'
-  import WavCodec, {IWavCodec} from '@/modules/wavCodec'
+import { Component, Vue } from 'vue-property-decorator'
+import WavCodec, { IWavCodec } from '@/modules/wavCodec'
 
-  enum SignalingType {
-    Online = 100,
-    Offline,
-    UserInfo,
-    Offer,
-    Answer,
-    Transfer,
+enum SignalingType {
+  Online = 100,
+  Offline,
+  UserInfo,
+  UserList,
+  Offer,
+  Answer,
+  HangUp,
+  Transfer,
+}
+
+interface Signaling {
+  [key: string]: any
+
+  command?: number
+  target?: number
+  data?: { [key: string]: any }
+}
+
+@Component
+export default class Recording extends Vue {
+  public isPaused: boolean = false
+  public isRecording: boolean = false
+  public stream?: MediaStream
+  public recorder?: MediaRecorder
+  public audioSources: any[] = []
+  public constraints: { [key: string]: any } = {
+    audio: true,
+    video: false,
   }
+  public dataChunks: Blob[] = []
+  public sampleRate: number = 8000
+  public bufferSize: number = 512
+  public duration: number = 0
+  public wavCodec?: IWavCodec
 
-  interface Signaling {
-    [key: string]: any
+  public ws?: WebSocket
+  public localPeer?: RTCPeerConnection
+  public userInfo: any = {}
 
-    command?: number
-    data?: { [key: string]: any }
-  }
+  public wsList: Array<{ [key: string]: any }> = []
 
-  @Component
-  export default class Recording extends Vue {
-    public isPaused: boolean = false
-    public isRecording: boolean = false
-    public stream?: MediaStream
-    public recorder?: MediaRecorder
-    public audioSources: any[] = []
-    public constraints: { [key: string]: any } = {
-      audio: true,
-      video: false
+  public async startRecord() {
+    this.stream = await navigator.mediaDevices.getUserMedia(this.constraints)
+    this.isRecording = true
+
+    // 创建音频环境，处理音频数据
+    // @ts-ignore
+    const wavCodec = this.wavCodec = new WavCodec(this.stream, {
+      sampleRate: this.sampleRate,
+      bufferSize: this.bufferSize,
+    })
+    wavCodec.onDuration = (allDuration: number) => {
+      this.duration = allDuration
     }
-    public dataChunks: Blob[] = []
-    public sampleRate: number = 8000
-    public bufferSize: number = 512
-    public duration: number = 0
-    public wavCodec?: IWavCodec
+    wavCodec.start()
 
-    public ws?: WebSocket
-    public localPeer?: RTCPeerConnection
-    public remotePeer?: RTCPeerConnection
-    public userInfo: any
 
-    public async startRecord() {
-      this.stream = await navigator.mediaDevices.getUserMedia(this.constraints)
-      this.isRecording = true
+    // 原生API
+    this.recorder = new MediaRecorder(this.stream)
+    this.recorder.onstart = (event: Event) => {
+      console.log('Recorder started')
+      this.calcRecordingTime()
+    }
+    this.recorder.onpause = (event: Event) => {
+      console.log('Recorder paused')
+    }
+    this.recorder.onstop = async (event: Event) => {
+      this.isRecording = false
+      if (!this.recorder || !this.stream) {
+        return
+      }
+      this.stream.getTracks().forEach((track) => track.stop())
 
-      // 创建音频环境，处理音频数据
-      // @ts-ignore
-      const wavCodec = this.wavCodec = new WavCodec(this.stream, {
-        sampleRate: this.sampleRate,
-        bufferSize: this.bufferSize
+      const blob = new Blob(this.dataChunks, { type: this.recorder.mimeType })
+      const dataUrl = URL.createObjectURL(blob)
+      console.log('Recorder stopped', blob, dataUrl)
+      this.audioSources.push({
+        dataUrl,
+        fileName: 'mediaRecorder' + new Date().toISOString() + '.' + this.getExt(),
       })
-      wavCodec.onDuration = (allDuration: number) => {
-        this.duration = allDuration
-      }
-      wavCodec.start()
 
+      // 最后翻译音频对象
+      this.recorder = undefined
+      this.stream = undefined
+    }
+    this.recorder.ondataavailable = async (event: BlobEvent) => {
+      const data = event.data
+      // const buffer = await new Response(data).arrayBuffer()
+      // console.log('ondataavailable', data, new Uint8Array(buffer))
+      this.dataChunks.push(data)
+    }
+    this.recorder.start(60)
+  }
 
-      // 原生API
-      this.recorder = new MediaRecorder(this.stream)
-      this.recorder.onstart = (event: Event) => {
-        console.log('Recorder started')
-        this.calcRecordingTime()
-      }
-      this.recorder.onpause = (event: Event) => {
-        console.log('Recorder paused')
-      }
-      this.recorder.onstop = async (event: Event) => {
-        this.isRecording = false
-        if (!this.recorder || !this.stream) {
-          return
-        }
-        this.stream.getTracks().forEach((track) => track.stop())
+  public async stopRecord() {
+    this.isPaused = false
+    this.recorder && this.recorder.stop()
 
-        const blob = new Blob(this.dataChunks, {type: this.recorder.mimeType})
-        const dataUrl = URL.createObjectURL(blob)
-        console.log('Recorder stopped', blob, dataUrl)
-        this.audioSources.push({
-          dataUrl,
-          fileName: 'mediaRecorder' + new Date().toISOString() + '.' + this.getExt()
-        })
+    if (this.wavCodec) {
+      this.wavCodec.stop()
+      const wavBlob = this.wavCodec.getBlob()
+      const wavAudio = {
+        dataUrl: URL.createObjectURL(wavBlob),
+        fileName: 'record' + new Date().toISOString() + '.wav',
+      }
+      this.audioSources.push(wavAudio)
+      console.log('this.wavCodec getBlob', wavBlob, wavAudio.dataUrl)
+    }
+  }
 
-        // 最后翻译音频对象
-        this.recorder = undefined
-        this.stream = undefined
-      }
-      this.recorder.ondataavailable = async (event: BlobEvent) => {
-        const data = event.data
-        // const buffer = await new Response(data).arrayBuffer()
-        // console.log('ondataavailable', data, new Uint8Array(buffer))
-        this.dataChunks.push(data)
-      }
-      this.recorder.start(60)
+  public toggleRecording() {
+    if (this.isRecording) {
+      this.stopRecord()
+    } else {
+      this.startRecord()
+    }
+  }
+
+  public togglePause() {
+    const wavCodec = this.wavCodec as IWavCodec
+    const recorder = this.recorder as MediaRecorder
+
+    if (this.isPaused) {
+      recorder.resume()
+      wavCodec.resume()
+      this.calcRecordingTime()
+    } else {
+      recorder.pause()
+      wavCodec.pause()
     }
 
-    public async stopRecord() {
-      this.isPaused = false
-      this.recorder && this.recorder.stop()
+    this.isPaused = !this.isPaused
+  }
 
-      if (this.wavCodec) {
-        this.wavCodec.stop()
-        const wavBlob = this.wavCodec.getBlob()
-        const wavAudio = {
-          dataUrl: URL.createObjectURL(wavBlob),
-          fileName: 'record' + new Date().toISOString() + '.wav'
-        }
-        this.audioSources.push(wavAudio)
-        console.log('this.wavCodec getBlob', wavBlob, wavAudio.dataUrl)
-      }
-    }
+  public calcRecordingTime() {
+    this.duration = 0
+  }
 
-    public toggleRecording() {
-      if (this.isRecording) {
-        this.stopRecord()
-      } else {
-        this.startRecord()
-      }
-    }
-
-    public togglePause() {
-      const wavCodec = this.wavCodec as IWavCodec
-      const recorder = this.recorder as MediaRecorder
-
-      if (this.isPaused) {
-        recorder.resume()
-        wavCodec.resume()
-        this.calcRecordingTime()
-      } else {
-        recorder.pause()
-        wavCodec.pause()
-      }
-
-      this.isPaused = !this.isPaused
-    }
-
-    public calcRecordingTime() {
-      this.duration = 0
-    }
-
-    public getExt(): string {
-      let ext: string = ''
-      if (!this.recorder) {
-        return ext
-      }
-
-      try {
-        const reg: RegExp = /(\w+)\/(?<ext>\w+)(;codecs)?/i
-        const res: RegExpMatchArray | null = this.recorder.mimeType.match(reg)
-
-        ext = (res && res.groups && res.groups.ext) || ext
-      } catch (e) {
-        console.error(e)
-      }
+  public getExt(): string {
+    let ext: string = ''
+    if (!this.recorder) {
       return ext
     }
 
-    public saveAsRecordData({dataUrl, fileName}: { [key: string]: string }): void {
-      if (!dataUrl) {
-        return
-      }
-      if (!fileName) {
-        fileName = new Date().toISOString()
-      }
+    try {
+      const reg: RegExp = /(\w+)\/(?<ext>\w+)(;codecs)?/i
+      const res: RegExpMatchArray | null = this.recorder.mimeType.match(reg)
 
-      const link = document.createElement('a')
-      link.href = dataUrl
-      link.download = fileName
+      ext = (res && res.groups && res.groups.ext) || ext
+    } catch (e) {
+      console.error(e)
+    }
+    return ext
+  }
 
-      link.click()
-      setTimeout(() => {
-        link.remove()
-      }, 100)
+  public saveAsRecordData({ dataUrl, fileName }: { [key: string]: string }): void {
+    if (!dataUrl) {
+      return
+    }
+    if (!fileName) {
+      fileName = new Date().toISOString()
     }
 
-    get durationLabel() {
-      return parseFloat((this.duration).toFixed(3))
+    const link = document.createElement('a')
+    link.href = dataUrl
+    link.download = fileName
+
+    link.click()
+    setTimeout(() => {
+      link.remove()
+    }, 100)
+  }
+
+  get durationLabel(): number {
+    return parseFloat((this.duration).toFixed(3))
+  }
+
+  get actionName(): string {
+    return this.isRecording ? 'Stop Record' : 'Start Record'
+  }
+
+  get pauseName(): string {
+    return this.isPaused ? 'Resume' : 'Paused'
+  }
+
+  public send(data: object) {
+    if (!this.ws || this.ws.readyState !== WebSocket.OPEN) {
+      return
     }
-
-    get actionName() {
-      return this.isRecording ? 'Stop Record' : 'Start Record'
-    }
-
-    get pauseName() {
-      return this.isPaused ? 'Resume' : 'Paused'
-    }
-
-    public send(data: object) {
-      if (!this.ws || this.ws.readyState !== WebSocket.OPEN) {
-        return
-      }
-      try {
-        this.ws.send(JSON.stringify(data))
-      } catch (e) {
-
-      }
-    }
-
-    public initWebSocket() {
-      const ws: WebSocket = (window as any).ws = this.ws = new WebSocket(`ws://${window.location.hostname}:7000/ws`)
-      ws.onmessage = async (event: MessageEvent) => {
-        let message: Signaling
-        try {
-          message = JSON.parse(event.data)
-        } catch (e) {
-          message = {}
-        }
-
-        // @ts-ignore
-        const {command, data} = message
-        switch (command) {
-          case SignalingType.Online:
-            console.log('Online:', data)
-            if (!this.userInfo) {
-              return
-            }
-            if (this.userInfo.userId === data.userId) {
-              return
-            }
-            if (!this.localPeer) {
-              return
-            }
-            await this.localPeer.setLocalDescription(await this.localPeer.createOffer())
-            this.send({
-              command: SignalingType.Offer,
-              data: {desc: this.localPeer.localDescription}
-            })
-            break
-          case SignalingType.Offline:
-            console.log('Offline:', data)
-            break
-          case SignalingType.UserInfo:
-            console.log('UserInfo:', data)
-            // 接收自己的连接信息
-            if (!this.userInfo) {
-              this.userInfo = {}
-            }
-            if (!this.userInfo.userId) {
-              this.userInfo = {...data}
-            }
-            break
-          case SignalingType.Offer:
-            console.log('Offer:', data)
-            if (!this.localPeer) {
-              return
-            }
-            await this.localPeer.setRemoteDescription(data.desc)
-            // this.stream.getTracks().forEach((track) => {
-            //   if (!this.localPeer) {
-            //     return
-            //   }
-            //   this.localPeer.addTrack(track, this.stream)
-            // })
-            await this.localPeer.setLocalDescription(await this.localPeer.createAnswer())
-            this.send({
-              command: SignalingType.Answer,
-              data: {desc: this.localPeer.localDescription}
-            })
-            break
-          case SignalingType.Answer:
-            console.log('Answer:', data)
-            if (!this.localPeer) {
-              return
-            }
-            await this.localPeer.setRemoteDescription(data.desc)
-            break
-          case SignalingType.Transfer:
-            console.log('Data:', data)
-            break
-          default:
-            console.warn('Unknown command:', command, data)
-        }
-      }
-      ws.onopen = (event: Event) => {
-        console.log('WebSocket opened!', event)
-        // 连接成功后，请求用户id等数据
-        this.send({
-          command: SignalingType.UserInfo
-        })
-      }
-      ws.onerror = (event: Event) => {
-        console.log('WebSocket error:', event)
-      }
-      ws.onclose = (event: CloseEvent) => {
-        console.log('WebSocket closed:', event)
-        // setTimeout(this.initWebSocket, 5 * 1000)
-      }
-    }
-
-    public beforeMount() {
-      const localPeer: RTCPeerConnection = this.localPeer = new RTCPeerConnection()
-      localPeer.onicecandidate = (event: RTCPeerConnectionIceEvent) => {
-        const {candidate} = event
-        console.log('localPeer onicecandidate', candidate)
-      }
-      localPeer.onnegotiationneeded = async () => {
-        try {
-          await localPeer.setLocalDescription(await localPeer.createOffer())
-          // send the offer to the other peer
-        } catch (err) {
-          console.error(err)
-        }
-      }
-      localPeer.ontrack = (event: RTCTrackEvent) => {
-        // don't set srcObject again if it is already set.
-        // if (remoteView.srcObject) return
-        // remoteView.srcObject = event.streams[0]
-        console.log('localPeer ontrack', event)
-      }
-
-
-      const remotePeer: RTCPeerConnection = this.remotePeer = new RTCPeerConnection()
-
-      this.initWebSocket()
+    try {
+      this.ws.send(JSON.stringify(data))
+    } catch (e) {
 
     }
   }
+
+  public wsOffline(data?: { [key: string]: any }) {
+    if (!data) {
+      return
+    }
+    for (let i = 0; i < this.wsList.length; i++) {
+      const item = this.wsList[i]
+      if (data.id === item.id) {
+        this.wsList.splice(i, 1)
+        break
+      }
+    }
+  }
+
+  public wsOnline(data?: { [key: string]: any }) {
+    data && this.wsList.push(data)
+  }
+
+  public async processCommand(message: Signaling) {
+    // @ts-ignore
+    const { command, data, target, source } = message
+
+    switch (command) {
+      case SignalingType.Online:
+        this.wsOnline(data)
+        console.log('Online:', data)
+        break
+      case SignalingType.Offline:
+        this.wsOffline(data)
+        console.log('Offline:', data)
+        break
+      case SignalingType.UserInfo:
+        console.log('UserInfo:', data)
+        // 接收自己的连接信息
+        this.userInfo = {
+          ...this.userInfo,
+          ...data,
+        }
+        this.send({
+          command: SignalingType.UserList,
+        })
+        break
+      case SignalingType.UserList:
+        console.log('UserList:', data)
+        this.wsList = [
+          ...this.wsList,
+          ...(data || {}).list,
+        ]
+        break
+      case SignalingType.Offer:
+        console.log('[Offer] data:%o, target:%d, source:%d', data, target, source)
+        if (!this.localPeer) {
+          this.localPeer = this.newRTCPeerConnection()
+        }
+        await this.localPeer.setRemoteDescription(new RTCSessionDescription((data || {}).desc))
+        await this.localPeer.setLocalDescription(await this.localPeer.createAnswer())
+        this.send({
+          command: SignalingType.Answer,
+          target: source,
+          data: {
+            desc: this.localPeer.localDescription,
+          },
+        })
+        break
+      case SignalingType.Answer:
+        console.log('[Answer] data:%o, target:%d, source:%d', data, target, source)
+        if (!this.localPeer) {
+          this.localPeer = this.newRTCPeerConnection()
+        }
+        await this.localPeer.setRemoteDescription(new RTCSessionDescription((data || {}).desc))
+        break
+      case SignalingType.HangUp:
+        console.log('[HangUp] data:%o, target:%d, source:%d', data, target, source)
+        this.hangUp()
+        break
+      case SignalingType.Transfer:
+        console.log('[Transfer] data:%o, target:%d, source:%d', data, target, source)
+        break
+      default:
+        console.warn('[Unknown] command:%d, target:%d, source:%d, data:%o', command, target, source, data)
+    }
+  }
+
+  public initWebSocket() {
+    const ws: WebSocket = (window as any).ws = this.ws = new WebSocket(`ws://${window.location.hostname}:7000/ws`)
+    ws.onmessage = (event: MessageEvent) => {
+      let message: Signaling
+      try {
+        message = JSON.parse(event.data)
+      } catch (e) {
+        message = {}
+      }
+
+      this.processCommand(message)
+    }
+    ws.onopen = (event: Event) => {
+      console.log('WebSocket opened!', event)
+    }
+    ws.onerror = (event: Event) => {
+      console.log('WebSocket error:', event)
+    }
+    ws.onclose = (event: CloseEvent) => {
+      console.log('WebSocket closed:', event)
+      // setTimeout(this.initWebSocket, 5 * 1000)
+    }
+  }
+
+  public beforeMount() {
+    this.initWebSocket()
+  }
+
+  public newRTCPeerConnection(): RTCPeerConnection {
+    this.localPeer = new RTCPeerConnection()
+    return this.localPeer
+  }
+
+  public async callPeer(user: { [key: string]: any }) {
+    console.log('callPeer', user.id)
+    this.newRTCPeerConnection()
+    if (!this.localPeer) {
+      return
+    }
+    await this.localPeer.setLocalDescription(await this.localPeer.createOffer())
+    this.send({
+      command: SignalingType.Offer,
+      target: user.id,
+      data: {
+        desc: this.localPeer.localDescription,
+      },
+    })
+  }
+
+  public hangUpPeer(user: { [key: string]: any }) {
+    console.log('hangUpPeer', user.id)
+    this.send({
+      command: SignalingType.HangUp,
+      target: user.id,
+    })
+  }
+
+  public hangUp() {
+    if (!this.localPeer) {
+      return
+    }
+
+    this.localPeer.ontrack = null
+    this.localPeer.oniceconnectionstatechange = null
+    this.localPeer.onsignalingstatechange = null
+    this.localPeer.onicegatheringstatechange = null
+
+    // Close the peer connection
+    this.localPeer.close()
+    this.localPeer = undefined
+
+  }
+}
 </script>
 
 <style scoped lang="scss">
@@ -360,5 +434,9 @@
     & audio {
       margin-right: $marginValue;
     }
+  }
+
+  .online-users {
+    width: 100%;
   }
 </style>
