a7136f42c7e7e26e9f1f6060e6bc7956520caa93
linfl <linfl@bfdx.com>
2019-09-24 17:26:41 +0800

    修改协议模块和webSocket初始化逻辑

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-
 import 'iview/dist/styles/iview.css'
+import { initWebSocket } from '@/protocol'
 
+initWebSocket()
 Vue.config.productionTip = false
 
 new Vue({
--- src/protocol/index.ts
+++ src/protocol/index.ts
@@ -1,41 +1,53 @@
-// @ts-ignore
 import { Transfer } from './rpc'
 
-type queryCallBack = (rpc: Transfer.IRpc) => void
+export * from './rpc'
+
+type queryCallBack = (data?: any) => void
 
 export interface SendOption {
   [key: string]: any
 
   callback?: queryCallBack
-  decodeCtx?: any
+  decoder?: any
 }
 
 const SocketCallOption: { [key: number]: SendOption } = {}
 
-const socket: WebSocket = new WebSocket(`ws://${window.location.hostname}:7000/ws`)
-socket.onmessage = (event: MessageEvent) => {
-  const data: Uint8Array = event.data
-  const message: Transfer.IRpc = Transfer.Rpc.decode(data)
-  const callOption: SendOption | undefined = SocketCallOption[message.qid]
-  if (callOption) {
-    if (callOption.decodeCtx) {
-      message.data = callOption.decodeCtx.decode(message.data)
-    }
-    if (callOption.callback) {
-      callOption.callback(message)
-    }
+let socket: WebSocket | undefined = undefined
+
+export function initWebSocket(url?: string) {
+  if (socket && (socket.readyState === socket.OPEN || socket.readyState === socket.CONNECTING)) {
+    return
   }
 
-  delete SocketCallOption[message.qid]
-}
-socket.onopen = (event: Event) => {
-  console.log('WebSocket opened!', event)
-  socket.binaryType = 'arraybuffer'
-}
-socket.onclose = (event: CloseEvent) => {
-  console.log('WebSocket closed:', event)
-}
+  if (!url) {
+    url = `ws://${window.location.hostname}:7000/ws`
+  }
+  socket = new WebSocket(url)
+  socket.onmessage = (event: MessageEvent) => {
+    const data: Uint8Array = event.data
+    console.log('onmessage:', data)
+    const message: Transfer.IRpc = Transfer.Rpc.decode(data)
+    const callOption: SendOption | undefined = SocketCallOption[message.qid]
+    if (callOption) {
+      if (callOption.decoder) {
+        message.data = callOption.decoder.decode(message.data)
+      }
+      if (callOption.callback) {
+        callOption.callback(message)
+      }
+    }
 
+    delete SocketCallOption[message.qid]
+  }
+  socket.onopen = (event: Event) => {
+    console.log('WebSocket opened!')
+    socket.binaryType = 'arraybuffer'
+  }
+  socket.onclose = (event: CloseEvent) => {
+    console.log('WebSocket closed:')
+  }
+}
 
 function socketIsOpen(): boolean {
   if (socket && socket.readyState === socket.OPEN) {
@@ -44,7 +56,7 @@ function socketIsOpen(): boolean {
   return false
 }
 
-function send(rpc: Transfer.IRpc, option: SendOption): void {
+export function sendMessage(rpc: Transfer.IRpc, option: SendOption): void {
   if (!socketIsOpen) {
     return
   }
@@ -64,7 +76,7 @@ function getQid(): number {
   return qid++
 }
 
-function newRpc(): Transfer.IRpc {
+export function newRpc(): Transfer.IRpc {
   const rpc = new Transfer.Rpc()
   rpc.qid = getQid()
 
