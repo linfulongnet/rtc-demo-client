d1dc9a4fbd63109b730f16290ceb83971ed020e7
linfl <linfl@bfdx.com>
2019-09-24 10:05:33 +0800

    upgrade dependencies version to latest

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   presets: [
-    '@vue/app'
+    ['@vue/app', { useBuiltIns: 'entry' }],
+    '@babel/preset-env'
+  ],
+  plugins: [
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-syntax-dynamic-import'
   ]
 }
